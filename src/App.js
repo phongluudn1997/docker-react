44170f41e69034f0c1f4071878a636976cce2ccc
luu <huynhpluu@gmail.com>
2022-06-13 22:51:22 +0700

    fixed: tests

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Changed asdf
+          Learn React
         </a>
       </header>
     </div>
